9b30d7e6dc9a6e61b5035dea91bf9e129af80c45
Florian Gaudin-Delrieu <florian.gaudindelrieu@gmail.com>
2017-01-26 23:13:05 +0100

    Add map full size

--- server.R
+++ server.R
@@ -3,9 +3,13 @@ source("02_fonctions.R")
 function(input, output, session) { 
 
   output$map <- renderLeaflet({
-    tmap_mode("view")
+    
     carte <- tmap_leaflet(creer_carte(input$prenom, input$dates[1], input$dates[2]))
     carte
     })
+  
+  output$histogramme <- renderPlot({
+    creer_histogramme(input$prenom, input$dates[1], input$dates[2])
+  })
 
 }
