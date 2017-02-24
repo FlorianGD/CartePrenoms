e2e9f0d5b9bee5db511cf471df0b47b3980fc2e1
Florian Gaudin-Delrieu <florian.gaudindelrieu@gmail.com>
2017-02-24 16:43:17 +0100

    Read data from csv and remove duplicate libraries calls

--- global.R
+++ global.R
@@ -1,11 +1,21 @@
-library(sp)
-library(rgdal)
-library(tmap)
-library(raster)
-library(dplyr)
 library(readr)
+library(rgdal)
+library(leaflet)
+options(OutDec= ",")
 
-naissances <- readRDS("data/recalc/naissances.rds")
-prenoms <- readRDS("data/recalc/prenoms.rds")
+naissances <- read_csv("data/recalc/naissances.csv", 
+                       col_types = cols(
+                         code_insee = col_character(),
+                         annee = col_integer(),
+                         naissances = col_double()
+                       ))
+prenoms <- read_csv("data/recalc/prenoms.csv", 
+                    col_types = cols(
+                      sexe = col_character(),
+                      prenom = col_character(),
+                      annee = col_integer(),
+                      code_insee = col_character(),
+                      nombre = col_double()
+                    ))
 france <- readOGR("data/recalc/departements", "departements-20140306-100m",
-                 stringsAsFactors = FALSE)
+                  stringsAsFactors = FALSE)
